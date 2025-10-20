bc171a24a44e7829adeaddc594206e2adef7fd9a
Nathan Joss (nat543207) <nate@jossfamily.com>
2025-10-19 22:24:38 -0700

    Allow instances of CRD types to be defined using `kubernetes.api.resources".
    This change enhances `kubernetes.api.resources` to include configuration
    sections for all CustomResourceDefinition-provided types that are
    defined directly through
    `kubernetes.api.resources.customResourceDefinitions`, imported with
    `imports`, or included in a consumed Helm chart.

--- modules/k8s.nix
+++ modules/k8s.nix
@@ -23,6 +23,25 @@ with lib; let
     then map moduleToAttrs value
     else value;
 
+  selfExtendingSubmodule = baseModule: extend: lib.mkOptionType {
+    name = "selfExtendingSubmodule";
+    merge = loc: defs: let
+      baseValue = (lib.types.submodule {
+        imports = [
+          { freeformType = lib.types.anything; }
+          baseModule
+        ];
+      }).merge loc defs;
+
+      extendedValue = (lib.types.submodule {
+        imports = [
+          baseModule
+          (extend baseValue)
+        ];
+      }).merge loc defs;
+    in extendedValue;
+  };
+
   apiOptions = { config, ... }: {
     options = {
       definitions = mkOption {
@@ -141,13 +160,13 @@ with lib; let
     in
     builtins.compareVersions v1 v2;
 
-  customResourceTypesByAttrName = zipAttrs (mapAttrsToList
+  byAttrName = customResourceTypes: zipAttrs (mapAttrsToList
     (_: resourceType: {
       ${resourceType.attrName} = resourceType;
     })
-    cfg.customTypes);
+    customResourceTypes);
 
-  customResourceTypesByAttrNameSortByVersion = mapAttrs
+  byAttrNameSortByVersion = customResourceTypes: mapAttrs
     (_: resourceTypes:
       reverseList (sort
         (
@@ -156,9 +175,9 @@ with lib; let
         )
         resourceTypes)
     )
-    customResourceTypesByAttrName;
+    (byAttrName customResourceTypes);
 
-  latestCustomResourceTypes = mapAttrsToList (_: last) customResourceTypesByAttrNameSortByVersion;
+  latest = customResourceTypes: mapAttrsToList (_: last) (byAttrNameSortByVersion customResourceTypes);
 
   customResourceModuleForType = config: ct: { name, ... }: {
     imports = getDefaults ct.name ct.group ct.version ct.kind;
@@ -192,7 +211,7 @@ with lib; let
     };
   };
 
-  customResourceOptions = (mapAttrsToList
+  customResourceOptions = customResourceTypes: (mapAttrsToList
     (_: ct: { config, ... }:
       let
         module = customResourceModuleForType config ct;
@@ -204,7 +223,7 @@ with lib; let
           default = { };
         };
       })
-    cfg.customTypes)
+    customResourceTypes)
   ++ (map
     (ct: { options, config, ... }:
       let
@@ -220,7 +239,7 @@ with lib; let
         config.resources.${ct.group}.${ct.version}.${ct.kind} =
           mkAliasDefinitions options.resources.${ct.attrName};
       })
-    latestCustomResourceTypes);
+    (latest customResourceTypes));
 
   coerceListOfSubmodulesToAttrs = submodule: keyFn:
     let
@@ -290,6 +309,33 @@ with lib; let
     if cfg.enableHashedNames && elem kind [ "ConfigMap" "Secret" ] then
       k8s.injectHashedNames object
     else object;
+
+  isCustomResourceDefinition = object: object.apiVersion == "apiextensions.k8s.io/v1" && object.kind == "CustomResourceDefinition";
+
+  typesFromCustomResourceDefinition = crd: map
+    (ver: let
+      syntheticRef = join
+        "."
+        (reverseList
+          ([ kind version ]
+            ++ (strSplit "." group)));
+      resource = syntheticRef;
+       group = crd.spec.group;
+       version = ver.name;
+       kind = crd.spec.names.kind;
+       name = toLower crd.spec.names.kind;
+    in {
+      inherit resource group version kind name;
+      attrName = name;
+      description = "Unchecked definitions of imported resource '${crd.spec.names.kind}'";
+      module = { freeformType = types.anything; };
+    })
+    crd.spec.versions;
+
+  typesFromCustomResourceDefinitionAttrs = concatMapAttrs
+    (_: crd: genAttrs'
+      (typesFromCustomResourceDefinition crd)
+      (customType: nameValuePair (gvkKeyFn customType) customType));
 in
 {
   imports = [ ./base.nix ];
@@ -332,13 +378,18 @@ in
     };
 
     api = mkOption {
-      type = types.submodule {
+      type = selfExtendingSubmodule {
         imports = [
           ./generated/v${cfg.version}.nix
           apiOptions
-        ]
-        ++ customResourceOptions;
-      };
+        ];
+      }
+      (super: {
+        imports = customResourceOptions
+          ((typesFromCustomResourceDefinitionAttrs
+            super.resources."apiextensions.k8s.io".v1.CustomResourceDefinition)
+            // cfg.customTypes);
+      });
       default = { };
     };
 
@@ -496,7 +547,9 @@ in
           (_: cr: {
             inherit (cr) name group version kind attrName;
           })
-          cfg.customTypes;
+          ((typesFromCustomResourceDefinitionAttrs
+            cfg.api.resources."apiextensions.k8s.io".v1.CustomResourceDefinition)
+            // cfg.customTypes);
 
         defaults = [{
           default = {
